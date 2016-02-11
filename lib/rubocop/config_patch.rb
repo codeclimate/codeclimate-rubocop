89f9a05ba838d775386d3ed336e40f1d9391d5f1
John Pignata <john@pignata.com>
2016-02-10 22:16:01 -0500

    Patch RuboCop::Config to warn on obsolete cops
    Instead of raising on the inclusion of an obsolete cop, write an error
    to STDERR. This is slightly more invisible than failing a build on an
    obsolete cop, but preferable as a user won't be blocked when we bump
    RuboCop on our engine. We'll write a changelog post advising users of
    the necessary changes to make and deprecate this patch in the coming
    weeks.

---
+++ lib/rubocop/config_patch.rb
@@ -0,0 +1,10 @@
+RuboCop::Config.class_eval do
+  def reject_obsolete_cops
+    RuboCop::Config::OBSOLETE_COPS.each do |cop_name, message|
+      next unless key?(cop_name) || key?(cop_name.split('/').last)
+      message += "\n(obsolete configuration found in #{loaded_path}, please" \
+        ' update it)'
+      $stderr.puts message
+    end
+  end
+end
