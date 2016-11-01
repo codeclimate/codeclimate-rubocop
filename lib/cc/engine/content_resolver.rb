892bc1c25603e9aed723ec97ef98b298a5be691b
Max Jacobson <max@hardscrabble.net>
2016-11-01 11:04:26 -0400

    Add test to assert that we have docs for cops
    We are missing 175 cops now...
    
    Adding them all will be tricky.

---
+++ lib/cc/engine/content_resolver.rb
@@ -0,0 +1,27 @@
+require "active_support/core_ext/string"
+
+module CC
+  module Engine
+    class ContentResolver
+      def initialize(cop_name)
+        @cop_name = cop_name
+      end
+
+      def content
+        @content ||= File.exist?(content_path) && File.read(content_path)
+      end
+
+      def content_path
+        @content_path ||= expand_config_path("contents/#{cop_name.sub("RuboCop::Cop::", "").underscore}.md")
+      end
+
+      private
+
+      attr_reader :cop_name
+
+      def expand_config_path(path)
+        File.expand_path("../../../../config/#{path}", __FILE__)
+      end
+    end
+  end
+end
